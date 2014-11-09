de276546393eb9d61f2f4ff49484fe442ac8f75b
chi42 <nwong@cyphort.com>
2014-11-08 17:12:19 -0800

    created database schema, works

---
+++ db/README.md
@@ -0,0 +1,6 @@
+to setup the db
+* install postgres
+* set postgres user password to password
+* psql -U postgres
+* "create database hero_hack;"
+* from bash, "psql -U postgres hero_hack < schema.sql"
---
+++ db/schema.sql
@@ -0,0 +1,45 @@
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+
+---CREATE PROCEDURAL LANGUAGE plpgsql;
+
+ALTER PROCEDURAL LANGUAGE plpgsql OWNER TO postgres;
+
+SET search_path = public, pg_catalog;
+SET default_tablespace = '';
+
+CREATE TABLE users (
+    id bigint NOT NULL UNIQUE,
+    login_name character varying UNIQUE NOT NULL,
+    name character varying NOT NULL,
+    weight integer NOT NULL,
+    height integer NOT NULL,
+    age integer NOT NULL,
+    gender character varying NOT NULL
+);
+---ALTER TABLE public.users OWNER TO postgres;
+
+CREATE TABLE workouts(
+    id bigint NOT NULL UNIQUE,
+    workout character varying NOT NULL,
+    date timestamp with time zone NOT NULL,
+    improve boolean,
+    notes character varying NOT NULL
+);
+---ALTER TABLE public.workouts OWNER TO postgres;
+
+CREATE TABLE sets(
+    id bigint NOT NULL UNIQUE,
+    workout_id bigint NOT NULL,
+    set_no integer NOT NULL,
+    weight integer NOT NULL,
+    count integer NOT NULL,
+    notes character varying NOT NULL
+);
+---ALTER TABLE public.sets OWNER TO postgres;
+
+GRANT ALL ON SCHEMA public TO postgres;
